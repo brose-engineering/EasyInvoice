1591450a70cb0e203b0e67efecbc8e6c58e769a3
Yannic Brose <info@brose-engineering.de>
2024-06-24 21:30:32 +0200

    invoice app created

--- Django/EasyInvoice/EasyInvoice/settings.py
+++ Django/EasyInvoice/EasyInvoice/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'customers',
     'homepage',
+    'invoice',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
