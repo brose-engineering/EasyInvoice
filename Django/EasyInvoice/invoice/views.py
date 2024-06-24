0f47bdf921a4fbb3fa9ce81421e551939327f1ed
Yannic Brose <info@brose-engineering.de>
2024-06-24 21:14:04 +0200

    invoice app created

--- Django/EasyInvoice/invoice/views.py
+++ Django/EasyInvoice/invoice/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from customers.models import Customer
 
 # Create your views here.
 def index(request):
-    pass
+    return HttpResponse(f"Hello there!")
