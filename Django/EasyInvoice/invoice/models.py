6f34dc0cd93294ce439b2e89868b2bba0f83e7bb
Yannic Brose <info@brose-engineering.de>
2024-07-12 21:29:55 +0200

    clean up work

--- Django/EasyInvoice/invoice/models.py
+++ Django/EasyInvoice/invoice/models.py
@@ -31,7 +31,33 @@ class InvoiceID(models.CharField):
 
 # Create your models here.
 class Invoice(models.Model):
+    """
+    The Invoice class defines a table in the database to store invoices.
+    """
+    # A unique identifier for each invoice, serving as the primary key.
     invoice_id = InvoiceID(primary_key=True)    
+    """
+    This attribute uniquely identifies each invoice and is used 
+    as the primary key for this model. It's an instance of InvoiceID, 
+    which could be a custom class or a built-in Python type like int or str.
+    """
+    # The customer who made the purchase, linked to the Customer model via a foreign key.
     customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
+    """
+    This attribute links each invoice to its corresponding customer. 
+    It's a foreign key that references the Customer model.
+    If the customer record is deleted, this association will be set to null (DO_NOTHING).
+    """
+    # The date when the invoice was created or issued.
     date = models.DateField()
+    """
+    This attribute represents the date when the invoice was created 
+    or issued. It's a standard DateField, which expects dates in YYYY-MM-DD format.
+    """
+    # A brief description of the product(s) being invoiced.
     product = models.CharField(max_length=256)
+    """
+    This attribute provides a short summary or description of the product(s) 
+    being invoiced. The maximum length is 256 characters, suitable for short descriptions.
+    """
+
--- Django/EasyInvoice/invoice/views.py
+++ Django/EasyInvoice/invoice/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render, get_object_or_404
-from customers.models import Customer
+from django.shortcuts import render
+from invoice.models import Invoice
 
 # Create your views here.
 def index(request):
-    return HttpResponse(f"Hello there!")
+    if request.method == "POST":
+        pass
+    else:
+        return render(request,'invoice/index.html',{
+            "title": "Easy Invoice - Rechnungen",
+            "invoices": Invoice.objects.all()
+        })
+    
+def new(request):
+    pass
