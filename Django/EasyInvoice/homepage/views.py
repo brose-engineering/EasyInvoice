b3e37bff86bd827bcbf01f5d3b27d4c74fe2913d
Yannic Brose <info@brose-engineering.de>
2024-08-09 16:33:39 +0200

    Kunden-Bereich sauber & funktional

--- Django/EasyInvoice/homepage/views.py
+++ Django/EasyInvoice/homepage/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'homepage/index.html', {
-        "title": "Easy Invoice - Das Rechnungsprogramm für Kleinunternehmen"
+        "title": "Easy Invoice",
+        "subtitle": "Das Rechnungsprogramm für Kleinunternehmer"
     })
